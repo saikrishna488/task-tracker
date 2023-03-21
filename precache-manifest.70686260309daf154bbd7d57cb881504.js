20f295fd8b8376a7b7940c70207bee54f27dd791
saikrishna488 <20p61a05h0@vbithyd.ac.in>
2023-03-21 17:48:02 +0530

    Updates

---
+++ precache-manifest.70686260309daf154bbd7d57cb881504.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0d6648f4ee635837186",
+    "url": "/task-tracker/static/css/main.8d79f976.chunk.css"
+  },
+  {
+    "revision": "e0d6648f4ee635837186",
+    "url": "/task-tracker/static/js/main.e0d6648f.chunk.js"
+  },
+  {
+    "revision": "4587b35573b7dda0a77d",
+    "url": "/task-tracker/static/js/1.4587b355.chunk.js"
+  },
+  {
+    "revision": "4ea73d5ac7db8c3f3474",
+    "url": "/task-tracker/static/js/2.4ea73d5a.chunk.js"
+  },
+  {
+    "revision": "04c5c63e4cb4b8f80ec5",
+    "url": "/task-tracker/static/js/runtime~main.04c5c63e.js"
+  },
+  {
+    "revision": "5ffd0ba3ac08815dac1c2b2df16a018c",
+    "url": "/task-tracker/index.html"
+  }
+];
